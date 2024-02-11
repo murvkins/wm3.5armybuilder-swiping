7db60945b45debbe07f7f36a284fd46ab8514cbf
murvkins <151895144+murvkins@users.noreply.github.com>
2024-02-10 19:23:44 -0700

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,8 +33,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "9b74015e2a03e2fa7c7b8cba14278868
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ab2add6d03865522de78bc75ffed4b29",
-"/": "ab2add6d03865522de78bc75ffed4b29",
+"index.html": "0b66149bcd7d4fe08fd96383d0a8b54d",
+"/": "0b66149bcd7d4fe08fd96383d0a8b54d",
 "main.dart.js": "7801c706885de3b61e4f1725e1714d31",
 "manifest.json": "87b25b2a403ceac8aa4c9ab5d8801049",
 "version.json": "aed377c0d3a7337f6fae5501aefa647b"};
